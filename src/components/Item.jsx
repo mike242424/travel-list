68ca15a60ec3bce995b088f3f87893c9ebace38e
mike242424 <mikebyers24@gmail.com>
2024-01-30 18:19:47 -0500

    setup form submit

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,6 +1,7 @@
 export default function Item({ item }) {
   return (
     <li>
+      <input type="checkbox" checked={item.packed} />
       <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
         {item.quantity} {item.description}
       </span>

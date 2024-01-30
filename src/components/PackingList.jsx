284c40eae73c4ad3355bd4108057d69c0d821cd5
mike242424 <mikebyers24@gmail.com>
2024-01-30 15:24:46 -0500

    rendered initial items list

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,3 +1,19 @@
+import Item from './Item';
+
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+  { id: 3, description: 'Charger', quantity: 1, packed: true },
+];
+
 export default function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }

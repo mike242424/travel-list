e3c05c2e19a191c10e57c836fe040af3e0d0bc4d
mike242424 <mikebyers24@gmail.com>
2024-01-30 15:04:18 -0500

    static structure complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
-export default App;
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip? 🩳</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}

2b264a516fc76dc1ffb4018a357bf29dad79ea1c
mike242424 <mikebyers24@gmail.com>
2024-01-30 15:34:44 -0500

    handled packed item condition with css

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,7 +1,7 @@
 export default function Item({ item }) {
   return (
     <li>
-      <span>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
         {item.quantity} {item.description}
       </span>
       <button>❌</button>
